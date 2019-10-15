68e501be1518663255dea2c022af753ed32a54b5
Billy Donahue <billy.donahue@mongodb.com>
2019-10-15 13:06:59 -0400

    benchmark

---
+++ src/mongo/util/stacktrace_bm.cpp
@@ -0,0 +1,84 @@
+/**
+ *    Copyright (C) 2019-present MongoDB, Inc.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the Server Side Public License, version 1,
+ *    as published by MongoDB, Inc.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    Server Side Public License for more details.
+ *
+ *    You should have received a copy of the Server Side Public License
+ *    along with this program. If not, see
+ *    <http://www.mongodb.com/licensing/server-side-public-license>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the Server Side Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+
+
+#include "mongo/platform/basic.h"
+
+#include <array>
+#include <cstdint>
+#include <functional>
+#include <limits>
+#include <random>
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include <benchmark/benchmark.h>
+
+#include "mongo/base/string_data.h"
+#include "mongo/util/decimal_counter.h"
+#include "mongo/util/stacktrace.h"
+
+namespace mongo {
+namespace {
+
+struct RecursionParam {
+    std::ostream& out;
+    std::vector<std::function<void(RecursionParam&)>> stack;
+};
+
+// Pops a callable and calls it. printStackTrace when we're out of callables.
+// Calls itself a few times to synthesize a nice big call stack.
+MONGO_COMPILER_NOINLINE int recursionTest(RecursionParam& p) {
+    if (p.stack.empty()) {
+        // I've come to invoke `stack` elements and test `printStackTrace()`,
+        // and I'm all out of `stack` elements.
+        printStackTrace(p.out);
+        return 0;
+    }
+    auto f = std::move(p.stack.back());
+    p.stack.pop_back();
+    f(p);
+    return 0;
+}
+
+void BM_Print(benchmark::State& state) {
+    size_t items = 0;
+    RecursionParam param{std::cout};
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(recursionTest(param));
+        ++items;
+    }
+    state.SetItemsProcessed(items);
+}
+
+BENCHMARK(BM_Print);
+
+}  // namespace
+}  // namespace mongo
