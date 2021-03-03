35b4d2007e19d4b1ba336b535d3b20473e512c6a
Billy Donahue <billy.donahue@mongodb.com>
2021-03-03 10:32:23 +0000

    test polymorphic vs nonpolymorphic intrusive_ptr performance

--- src/mongo/base/status_bm.cpp
+++ src/mongo/base/status_bm.cpp
@@ -33,6 +33,8 @@
 #include <string>
 #include <vector>
 
+#include <boost/intrusive_ptr.hpp>
+
 #include "mongo/base/string_data.h"
 #include "mongo/base/status.h"
 #include "mongo/util/processinfo.h"
@@ -78,6 +80,52 @@ void BM_StatusRefUnref(benchmark::State& state) {
 
 BENCHMARK(BM_StatusRefUnref)
         ->ThreadRange(1, 4);
+
+template <typename D>
+struct TestRefCountable {
+    friend void intrusive_ptr_add_ref(const TestRefCountable* ptr) {
+        ptr->_count.fetch_add(1, std::memory_order_relaxed);
+    };
+
+    friend void intrusive_ptr_release(const TestRefCountable* ptr) {
+        if (ptr->_count.fetch_sub(1, std::memory_order_acq_rel) == 1)
+            delete static_cast<const D*>(ptr);
+    };
+
+    mutable std::atomic<uint32_t> _count;  // NOLINT
+};
+
+struct Nonpolymorphic : TestRefCountable<Nonpolymorphic> {
+};
+
+struct Polymorphic : TestRefCountable<Polymorphic> {
+    virtual ~Polymorphic() = default;
+};
+
+void BM_NonpolymorphicRefUnref(benchmark::State& state) {
+    static boost::intrusive_ptr<Nonpolymorphic> s{new Nonpolymorphic{}};
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(boost::intrusive_ptr<Nonpolymorphic>{s});
+    }
+}
+
+BENCHMARK(BM_NonpolymorphicRefUnref)
+        ->ThreadRange(1, 4);
+
+/**
+ * Curious: Does a virtual dtor mean a slower ref/unref cycle?
+ * The count is then at offset 8 rather than 0, so maybe!
+ */
+void BM_PolymorphicRefUnref(benchmark::State& state) {
+    static boost::intrusive_ptr<Polymorphic> s{new Polymorphic{}};
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(boost::intrusive_ptr<Polymorphic>{s});
+    }
+}
+
+BENCHMARK(BM_PolymorphicRefUnref)
+        ->ThreadRange(1, 4);
+
 
 }  // namespace
 }  // namespace mongo
